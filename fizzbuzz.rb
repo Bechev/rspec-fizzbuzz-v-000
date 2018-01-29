3960eab928f767cfe3da8d8d24ae820fe6116013
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-01-29 02:27:52 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,8 @@
-def fizzbuzz
+def fizzbuzz(int)
+  if int % 3 == 0
+    output = "fizz"
+  elseif int % 5 == 0
+    output = "buzz"
+  elseif (int % 3 == 0) && (int % 5 == 0)
+    output = "fizzbuzz"
 end
